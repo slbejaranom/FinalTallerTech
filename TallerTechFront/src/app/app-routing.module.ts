7c116b4c054d86855473c46f142712fef80a365d
SEBASTIAN LEONARDO BEJARANO MEDELLIN <1030671497@bolivar.com>
2021-07-19 01:33:22 -0500

    Database scripts and front end

--- TallerTechFront/src/app/app-routing.module.ts
+++ TallerTechFront/src/app/app-routing.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { InicialComponent } from './components/inicial/inicial.component';
 
-const routes: Routes = [
-  {
-    path:'',
-    component: InicialComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- TallerTechFront/src/app/app.module.ts
+++ TallerTechFront/src/app/app.module.ts
@@ -3,20 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InicialComponent } from './components/inicial/inicial.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatGridList } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    InicialComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatGridList
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
